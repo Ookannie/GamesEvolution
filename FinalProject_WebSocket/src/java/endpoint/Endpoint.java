10c7a9536df9abbacfc5b903d01dd72fdc06f958
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-05-24 20:38:10 -0700

    WebSocket

--- FinalProject_WebSocket/src/java/endpoint/Endpoint.java
+++ FinalProject_WebSocket/src/java/endpoint/Endpoint.java
@@ -5,7 +5,16 @@
  */
 package endpoint;
 
-
+import Controladores.VideojuegosJpaController;
+import Controladores.exceptions.NonexistentEntityException;
+import Entidades.Inventariovideojuegos;
+import Entidades.Rentasvideojuegos;
+import Entidades.Videojuegos;
+import Fachadas.PersistenciaBD;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -19,11 +28,13 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/endpoint")
 public class Endpoint {
 
-    
-    
+    VideojuegosJpaController facade = new VideojuegosJpaController();
+
     @OnMessage
     public String onMessage(String message) {
-        System.out.println(message);
+
+        updateVideogame(message);
+
         return null;
     }
 
@@ -31,15 +42,67 @@ public class Endpoint {
     public void onOpen() {
     }
 
-
     @OnError
     public void onError(Throwable t) {
-        
+
     }
 
     @OnClose
     public void onClose() {
         System.out.println("The user has been disconnected");
+        facade.closeFactory();
     }
-    
+
+    private void updateVideogame(String message) {
+        try {
+        String[] values = splitMessage(message);
+        Videojuegos videogame = getVideogame(values);
+
+            facade.edit(videogame);
+        } catch (NonexistentEntityException ex) {
+            System.out.println("aaaaaa");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private Videojuegos getVideogame(String[] values) {
+        Videojuegos videogame = new Videojuegos();
+        videogame.setNumCatalogo(values[0]);
+        videogame.setTitulo(values[1]);
+        videogame.setGenero(values[2]);
+        videogame.setClasificacion(values[3]);
+        videogame.setConsola(values[4]);
+        videogame.setFabricante(values[5]);
+        videogame.setVersion(Integer.parseInt(values[6]));
+        videogame.setRentasvideojuegosList(new ArrayList<Rentasvideojuegos>());
+
+        return videogame;
+    }
+
+    private String[] splitMessage(String message) {
+        String[] values = new String[7];
+        String[] attributes = message.split(",");
+
+        for (int i = 0; i < attributes.length; i++) {
+            String[] valuesTemp = attributes[i].split(":");
+            values[i] = getCharacters(valuesTemp[1]);
+            System.out.println(values[i]);
+
+        }
+
+        return values;
+    }
+
+    private String getCharacters(String string) {
+        String temp = "";
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i)) || Character.isLetter(string.charAt(i))) {
+                temp += String.valueOf(string.charAt(i));
+            }
+        }
+
+        return temp;
+    }
+
 }
